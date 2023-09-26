2ad4b969573eb1cafd96138870960039c1df870e
Roope <roopejuuz@gmail.com>
2023-09-26 04:19:38 +0300

    todolist+delete

---
+++ todolist+delete/src/App.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index))
+    }
+
+  return (
+    <div className="App">
+      <form onSubmit={addTodo}>
+        Description:<input type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
+        Date:<input type="text" name="date" value={todo.date} onChange={inputChanged}/>
+        <input type="submit" value="Add"/>
+      </form>
+      <table>
+        <thead class="bold">
+          <tr>
+            <td>Description</td>
+            <td>Date</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+          todos.map((todo, index) =>
+            <tr key={index}>
+              <td>{todo.desc}</td>
+              <td>{todo.date}</td>
+              <td><button onClick={() => deleteTodo(index)}>Delete</button></td>
+            </tr>
+            )
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
